228c9ce9d6a16fe3c2999f930de8aa6fbc639d46
Mohit Kumar <mohitkumardhiman0233@gmail.com>
2020-07-20 19:23:35 +0530

    Comit 8

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit, Output, EventEmitter, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatDialog } from '@angular/material/dialog';
-import { ActivatedRoute, Router } from '@angular/router';
-import { takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { ActivatedRoute, Router, Params, NavigationEnd } from '@angular/router';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
   selector: 'app-navbar',
@@ -15,35 +14,28 @@ export class NavbarComponent implements OnInit {
 
   flag: boolean;
   currentDialog: MatDialogRef<any> = null;
-  //destroy = new Subject<any>();
-  //router: Router;
 
   //Dialog init
   constructor(public dialog: MatDialog, private route: ActivatedRoute, private router: Router) {
-    //route.params.pipe(takeUntil(this.destroy)).subscribe(params => {
-    //  if (this.currentDialog) {
-    //    this.currentDialog.close();
-    //  }
-
-      //this.currentDialog = matDialog.open(DialogComponent, {
-      //  data: {tabvalue},
-    //})
-  //})
-      //this.router = router;
-      //console.log(this.router)
-      //console.log(params)
-      //console.log(params.id)
+    //console.log("♥ router", this.router.url);
+    //console.log("this.currentDialog",this.currentDialog);
+    //if (this.currentDialog != 'null')
+    //  console.log("get dialog state", this.currentDialog.getState);
+    if (this.router.url == '/login') {
+      this.openDialog(0);
+    } else if (this.router.url == '/signin') {
+      this.openDialog(1);
+    }
 
    }
-
-
-  //ngOnDestroy() {
-  //  this.destroy.next()
-  //}
 
   //dialog properties for init dialog
   openDialog(tabvalue: number) {
     console.log(tabvalue);
+    //if (!this.dialog.openDialogs || !this.dialog.openDialogs.length) return;
+    //console.log(this.dialog.openDialogs);
+    //console.log(this.dialog.openDialogs.length);
+    if (this.dialog.openDialogs.length != 0) return;
     this.currentDialog = this.dialog.open(DialogComponent, {
       data: {tabvalue},
     });
@@ -62,32 +54,6 @@ export class NavbarComponent implements OnInit {
   //Init the value of flag here
   ngOnInit(): void {
     this.flag = true;
-    this.route.params.subscribe(params => {
-      /*if (params["login"] == true) {
-        this.openDialog(0);
-      }
-      if (params["signup"] == true) {
-        this.openDialog(1);
-      }*/
-      console.log("route", this.route);
-      console.log("params", params);
-      console.log("snapshot", this.route.snapshot);
-      console.log("URL", this.route.url);
-      console.log("querryParams", this.route.queryParams);
-      console.log("component", this.route.component);
-      console.log("FirstChild", this.route.firstChild);
-      console.log("children", this.route.children);
-      console.log("this", this);
-      console.log("paramsMap", this.route.paramMap);
-      console.log("data", this.route.data)
-      console.log("fragment", this.route.fragment);
-      console.log("outlet", this.route.outlet);
-      console.log("pathFromRoot", this.route.pathFromRoot);
-      console.log("parent", this.route.parent);
-      console.log("queryParamMap", this.route.queryParamMap)
-      console.log("root", this.route.root);
-      console.log("routeconfig", this.route.routeConfig);
-    });
   }
 
 }
@@ -99,9 +65,27 @@ export class NavbarComponent implements OnInit {
 })
 export class DialogComponent {
   tabs = ['Login', 'Sign in'];
+  links = [
+    { link: 'login', tab: this.tabs[0] },
+    { link: 'signin', tab: this.tabs[1] }
+  ];
+  activeLink = this.links[0];
+  background: ThemePalette = undefined;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
 
   // tabvalue have only two values 0 -> tabs[0] -> Login and 1 -> tabs[1] -> SignIn
+  c() {
+    console.log("working");
+    if (this.selected == 0) {
+      this.selected = 1;
+    }
+    else {
+      this.selected = 0;
+    }
+  }
+  onC(a) {
+    console.log("one zero", a);
+  }
   selected = this.data.tabvalue
 }
